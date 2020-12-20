948aa6ff5242a2168514dc87e47858ea9c8c52a5
Adi <68931532+HimalayanPanda@users.noreply.github.com>
2020-12-20 17:53:14 +1100

    completed basic functionalities

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
+let addTaskButton = document.getElementById('addTask');
+let taskContainer = document.getElementById('taskContainer');
+let inputField = document.getElementById('inputField');
 
+addTaskButton.addEventListener('click', function(){
+    var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    paragraph.innerText = inputField.value;
+    taskContainer.appendChild(paragraph);
+    inputField.value = "";
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    })
+    paragraph.addEventListener('dblclick', function(){
+        taskContainer.removeChild(paragraph);
+    })
+})
