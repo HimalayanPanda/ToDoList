2273a8af29a25476fdd389cac84de6494b06e1f5
Adi <68931532+HimalayanPanda@users.noreply.github.com>
2020-12-23 11:24:50 +1100

    Added bg image and date

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let addTaskButton = document.getElementById('addTask');
 let taskContainer = document.getElementById('taskContainer');
 let inputField = document.getElementById('inputField');
 
+var dt = new Date();
+document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +"/"+ (("0"+(dt.getMonth()+1)).slice(-2)) +"/"+ (dt.getFullYear());
+
 addTaskButton.addEventListener('click', function(){
     var paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
